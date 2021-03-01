a688e7c91dda2a9bc62108d87481d2c547c6e076
unknown <ssong8154@gmail.com>
2021-03-01 19:51:10 +0900

    백엔드 로그인 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,10 +11,14 @@ function login() {
     id: id.value,
     psword: psword.value,
     };
-    console.log(req);
-    console.log(JSON.stringify(req));
+    
     fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
         body: JSON.stringify(req)
-    })
+    }).then((res) => res.json());
+      .then((res) => console.log(res.json()));
 };
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,44 @@
 "use strict"
 
-const hello = (req, res) => {
-    res.render("home/index");
-};
-   
-const login = (req, res) => {
-    res.render("home/login");
-};
+const users = {
+    id: ["song", "kim1"],
+    psword: ["123", "1234"],
+}
+
+const output = {
+
+    hello: (req, res) => {
+        res.render("home/index");
+    },
+       
+    login: (req, res) => {
+        res.render("home/login");
+    },
+}
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+            psword = req.body.psword;
+
+        if (users.id.includes(id)){
+          const idx = users.id.indexOf(id);
+          if (users.psword[idx] === psword) {
+              return res.json({
+                  success: true,
+              });
+          }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다."
+        })
+    },
+}
    
 
 module.exports = {
-    hello,
-    login,
+    output,
+    process,
 }
